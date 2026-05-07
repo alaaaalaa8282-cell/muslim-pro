1793b722602045ae0744ce8a66e81e3f7872b884
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-05-08 01:59:19 +0300

    Update IslamicProHelper.java

--- app/src/main/java/com/AbuMohamed/IslamicProHelper.java
+++ app/src/main/java/com/AbuMohamed/IslamicProHelper.java
@@ -1,8 +1,9 @@
 package com.AbuMohamed.App;
 
-
 public class IslamicProHelper {
     public static final String PREFS_NAME = "com.AbuMohamed";
     public static final String USER_CITY = "MyCurrentCity";
-    public static String MAP_KEY="AIzaSyBbYnWfoDdGCtyiR0UWBDoXZeat7y7X1Mc";
+    public static final String USER_LAT = "MyLat";
+    public static final String USER_LNG = "MyLng";
+    public static String MAP_KEY = "AIzaSyBbYnWfoDdGCtyiR0UWBDoXZeat7y7X1Mc";
 }
