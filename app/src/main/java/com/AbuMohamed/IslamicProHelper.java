3580b7a9c268f507a4c06dc410e8fe94a274da15
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-05-08 09:33:33 +0300

    Delete app/src/main/java/com/AbuMohamed/IslamicProHelper.java

--- app/src/main/java/com/AbuMohamed/IslamicProHelper.java
+++
@@ -1,9 +0,0 @@
-package com.AbuMohamed;
-
-public class IslamicProHelper {
-    public static final String PREFS_NAME = "com.AbuMohamed";
-    public static final String USER_CITY = "MyCurrentCity";
-    public static final String USER_LAT = "MyLat";
-    public static final String USER_LNG = "MyLng";
-    public static String MAP_KEY = "AIzaSyBbYnWfoDdGCtyiR0UWBDoXZeat7y7X1Mc";
-}
