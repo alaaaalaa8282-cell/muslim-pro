1814132905c7ddc5a7e8b32e6598e3c7ad45d229
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-05-08 12:09:05 +0300

    Update SplashScreenActivity.java

--- app/src/main/java/com/AbuMohamed/SplashScreenActivity.java
+++ app/src/main/java/com/AbuMohamed/SplashScreenActivity.java
@@ -212,7 +212,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     }
 
     private void prayerTimeParsing(final String mDate, final String locat) {
-        StringRequest request = new StringRequest(Request.Method.GET, Apis.prayerTime + mDate + "&city=" + locat + "&country=EG",
+        StringRequest request = new StringRequest(Request.Method.GET, Apis.prayerTime + mDate + "&latitude=" + latitude + "&longitude=" + longitude, new Response.Listener<String>() {
             @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onResponse(String response) {
