4b1608702349d5f6c9c136f37c6f56ab56bb5e25
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-05-08 11:54:40 +0300

    Update Apis.java

--- app/src/main/java/com/AbuMohamed/App/Apis.java
+++ app/src/main/java/com/AbuMohamed/App/Apis.java
@@ -1,15 +1,15 @@
 package com.AbuMohamed.App;
 
 public class Apis {
-    public static String BASEUrl = "https://muslimsolution.website/api_v1/";
-    public static String prayerTime = BASEUrl + "ptfix.php?date=";
-    public static String ramadan = BASEUrl + "/ramadan.php?lat=";
+    public static String BASEUrl = "https://api.aladhan.com/v1/";
+    public static String prayerTime = BASEUrl + "timingsByCity?method=5&date=";
+    public static String ramadan = BASEUrl + "calendar?method=5&latitude=";
     public static String tvUrl = "http://news.iotait.tech/apk/items.json";
-    public static String _99NmaesUrl = BASEUrl + "99names.php?all";
-    public static String islamicCalenderHolidaysUrl = BASEUrl + "holiday.php";
-    public static String suraList = BASEUrl + "quran.php?surah";
-    public static String suraAyahInArabic = BASEUrl+"quran.php?surahId=";
-    public static String suraAyahInEnglish = BASEUrl+"quran.php?surahEnglishId=";
+    public static String _99NmaesUrl = BASEUrl + "asmaUlHusna";
+    public static String islamicCalendarHolidaysUrl = BASEUrl + "currentIslamicMonth";
+    public static String suraList = "https://api.alquran.cloud/v1/surah";
+    public static String suraAyahInArabic = "https://api.alquran.cloud/v1/surah/";
+    public static String suraAyahInEnglish = "https://api.alquran.cloud/v1/surah/";
     public static String ayahAudio = "https://cdn.alquran.cloud/media/audio/ayah/ar.alafasy/";
-    public static String holyDaysUrl = BASEUrl + "holiday.php";
+    public static String holyDaysUrl = BASEUrl + "currentIslamicMonth";
 }
