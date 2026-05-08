4cc0a03614af656c0796ed27ac2a2d2cba55ce32
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-05-08 12:10:13 +0300

    Update Apis.java

--- app/src/main/java/com/AbuMohamed/App/Apis.java
+++ app/src/main/java/com/AbuMohamed/App/Apis.java
@@ -2,7 +2,7 @@ package com.AbuMohamed.App;
 
 public class Apis {
     public static String BASEUrl = "https://api.aladhan.com/v1/";
-    public static String prayerTime = BASEUrl + "timingsByCity?method=5&date=";
+    public static String prayerTime = BASEUrl + "timings?method=5&date=";
     public static String ramadan = BASEUrl + "calendar?method=5&latitude=";
     public static String tvUrl = "http://news.iotait.tech/apk/items.json";
     public static String _99NmaesUrl = BASEUrl + "asmaUlHusna";
